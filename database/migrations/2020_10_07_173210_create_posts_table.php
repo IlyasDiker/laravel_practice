d1361603452a110c7efae2e63d5a1c2bd1f203f4
IlyasDiker <ilyasbenhssine@gmail.com>
2020-10-07 19:04:25 +0100

    add migrations up & down

--- database/migrations/2020_10_07_173210_create_posts_table.php
+++ database/migrations/2020_10_07_173210_create_posts_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class PostTable extends Migration
+class CreatePostsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,12 @@ class PostTable extends Migration
      */
     public function up()
     {
-        //
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 120);
+            $table->string('content');
+            $table->timestamps();
+        });
     }
 
     /**
@@ -23,6 +28,6 @@ class PostTable extends Migration
      */
     public function down()
     {
-        //
+        Schema::dropIfExists('posts');
     }
 }
