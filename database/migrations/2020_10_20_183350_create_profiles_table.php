f3a15347606317efe9c3204c43430194c1d45c34
IlyasDiker <ilyasbenhssine@gmail.com>
2020-10-20 20:24:51 +0100

    add authors & profile and linking them

---
+++ database/migrations/2020_10_20_183350_create_profiles_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProfilesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('profiles', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('author_id');
+            $table->timestamps();
+        });
+        Schema::table('profiles', function (Blueprint $table) {
+            $table->foreign('author_id')->references('id')->on('authors');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('profiles');
+    }
+}
